d9cce03ea2cad5ce241497c92f4b7e2e632d52f4
vitoriasantosgoncalves10466-web <vitoriasantosgoncalves10466@gmail.com>
2025-08-26 11:57:27 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,28 @@
+# alunos = ["Ana", "Pedro", "Lucas", "Maria"]
+
+# for item in alunos: 
+#     print("Os alunos são:", item)
+
+# #Função range
+# for i in range(5): #Crie 5 números i significa apresentar em sequência
+#     print(i)
+
+# for i in range(2, 6): #Estrutura de repetição que inicia no 2 e termina no 6
+#     print(i)
+
+# for i in range(0, 10, 2): #Estrutura para pular de dois em dois 
+#     print(i)
+
+#Função len
+# alunos = ["Ana", "Pedro", "Lucas", "Maria"]
+# print(len(alunos))
+
+# palavra = "Python"  #Contabiliza as letras de uma palavra
+# print(len(palavra))
+
+# for letra in "Python": #Tipo de dado em que se é possível percorrer
+#     print("Letra: ", letra)
+
+for numero in range(1, 6):
+    print("Número: ", numero)
+
